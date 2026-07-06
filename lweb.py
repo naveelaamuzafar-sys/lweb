ac44c30322b5e71d44a75382500e4f779726c68a
naveelaamuzafar-sys <naveelaamuzafar@gmail.com>
2026-07-06 10:54:58 +0530

    Add files via upload

---
+++ lweb.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="For My Love ❤️", page_icon="❤️")
+
+st.title("❤️ To My Favorite Person ❤️")
+
+st.write("Hi Love,")
+st.write("I made this little website just for you.")
+st.write("Every moment with you is my favorite memory. ❤️")
+
+if st.button("💌 Open My Heart"):
+    st.balloons()
+    st.success("I love you more than words can ever express. Thank you for being you. ❤️")
+    
+
+st.image(r"C:\Users\C_World\Downloads\w.jpeg", width=500)
+
